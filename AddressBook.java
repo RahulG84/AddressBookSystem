3fae2170e2ca4896bac7e25929fcdf6d1d9c4b00
Your Name <rahul4girgal@gmail.com>
2022-07-06 19:23:42 +0530

    Added : Ability to create a Contacts in Address

---
+++ AddressBook.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class AddressBook {
+    //object of Contact class
+    Contact contact = new Contact();
+    //object of Scanner class for User Input
+    Scanner scan = new Scanner(System.in);
+
+    //Method for Adding Contact
+    public void addContact() {
+        System.out.println("Enter the First Name: ");
+        String firstName = scan.nextLine();
+        System.out.println("Enter the Last Name: ");
+        String lastName = scan.nextLine();
+        System.out.println("Enter the Mobile No: ");
+        String mobileNo = scan.nextLine();
+        System.out.println("Enter the Address: ");
+        String address = scan.nextLine();
+        System.out.println("Enter the City: ");
+        String city = scan.nextLine();
+        System.out.println("Enter the State: ");
+        String state = scan.nextLine();
+        System.out.println("Enter the Zip: ");
+        String zip = scan.nextLine();
+        System.out.println("Enter the Email: ");
+        String email = scan.nextLine();
+        //calling set function with reference name contact
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        contact.setMobileNo(mobileNo);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setEmail(email);
+        System.out.println(contact);
+    }
+}
---
+++ AddressBookSystem.java
@@ -0,0 +1,9 @@
+public class AddressBookSystem {
+    // main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+    }
+}
